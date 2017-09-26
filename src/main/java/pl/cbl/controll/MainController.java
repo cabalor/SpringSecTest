3acc4b57e2bc779df2995bd363728b2f92a2c992
cabalor <cabalor@gmail.com>
2017-09-26 18:53:24 +0200

    working more tomorrow

--- src/main/java/pl/cbl/controll/MainController.java
+++ src/main/java/pl/cbl/controll/MainController.java
@@ -1,5 +1,19 @@
 package pl.cbl.controll;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
 
+	
+	@RequestMapping("/")
+	public String start(Model m) {
+		
+		m.addAttribute("msg", "this is kraken, hello here");
+		return "main";
+	}
+	
+	
 }
