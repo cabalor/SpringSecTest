a9e56541a4532846317b6300c7bb3b76fc2fd1fa
cabalor <cabalor@gmail.com>
2017-09-27 17:28:22 +0200

    user added

--- src/main/java/pl/cbl/security/SecuCfg.java
+++ src/main/java/pl/cbl/security/SecuCfg.java
@@ -1,10 +1,19 @@
 package pl.cbl.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecuCfg {
+public class SecuCfg extends WebSecurityConfigurerAdapter{
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder authoryzation) throws Exception {
+        authoryzation.inMemoryAuthentication().withUser("cbl").password("password").roles("USER"); // with this method we can create an user and set a login and password for him
+    }
+	
+	
+	
 }
