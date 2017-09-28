c0b76f525f068660eb22b75a48b04f015c6e254d
cabalor <cabalor@gmail.com>
2017-09-28 18:08:28 +0200

    secu

--- src/main/java/pl/cbl/security/SecuCfg.java
+++ src/main/java/pl/cbl/security/SecuCfg.java
@@ -2,6 +2,7 @@ package pl.cbl.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,10 +11,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecuCfg extends WebSecurityConfigurerAdapter{
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder authoryzation) throws Exception {
-        authoryzation.inMemoryAuthentication().withUser("cbl").password("password").roles("USER"); // with this method we can create an user and set a login and password for him
+	protected void configure(HttpSecurity http) throws Exception {
+        	http
+        		.authorizeRequests().antMatchers("/")
+        		.permitAll().anyRequest().authenticated()
+        		.and().formLogin();
     }
-	
+	@Override
+	protected void configure(AuthenticationManagerBuilder authoryzation) throws Exception {
+		
+		
+		
+		authoryzation.inMemoryAuthentication().withUser("cbl").password("password").roles("USER"); // with this method we can create an user and set a login and password for him
+    }
 	
 	
 }
